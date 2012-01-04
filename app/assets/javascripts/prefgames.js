21822978770521eff6241f4b01f9ab4e12a40b7e
Bratislav Petrovic <bratislav.petrovic@gmail.com>
2012-01-04 23:44:14 +0100

    card is finaly shown

--- app/assets/javascripts/prefgames.js
+++ app/assets/javascripts/prefgames.js
@@ -1,12 +1,87 @@
+/* crating Card sprite */
+
+function newCard() {
+    var card = {
+        canvas: 0,
+	ctx: 0, 
+	x: 0,
+        y: 0,
+        animation: 0,
+        currentFrame: 0,
+        width: 72,
+        height: 96,
+        image: 0,
+        currentStep: 0,
+        is_ready: 0,
+        initCard: function(canvas,x,y,width,height,img_file){
+            card.is_ready = false;
+            card.x = x;
+            card.y = y;
+            card.width = width;
+            card.height = height;
+            card.canvas = canvas; // ? nije mi bas jasno zasto 
+            card.canvas.setAttribute('width',width);
+            card.canvas.setAttribute('height',height);
+            card.ctx = card.canvas.getContext('2d');
+            card.loadImage(img_file); 
+        },
+        loadImage: function(img_file){
+            card.image = new Image();
+            card.image.onload = function () { card.is_ready = true; };
+            card.image.src = img_file;
+        },
+        drawImage: function(){
+            card.ctx.clearRect(0,0,card.width,card.height); // clear previous frame
+	    if ( card.is_ready) {
+                var srcX = 0;
+		var srcY = 0;
+                var srcW = 90;
+                var srcH = 70;
+                card.ctx.drawImage(card.image,0,0,72,96,0,0,card.width,card.height); // drawimage  
+            }
+        },
+        
+    };
+    return card;
+};
+
 /* main table object */
 var cardT = {
-    canvas_el : 0,
+    cardw : 72,  // initial card width
+    cardh : 96, // initial card height
+    canvas_el : 0, // canvas
+    ctx : 0, // context,
+    card : 0, // card object /
     setup: function(){
-      cardT.canvas_el = $('#main')[0]
+	cardT.canvas_el = $('#main').get(0);
+        cardT.ctx = cardT.canvas_el.getContext('2d');
+        /* new canvas for card */
+        var new_canvas = document.createElement('canvas');
+        cardT.card = newCard();
+        cardT.card.initCard(new_canvas,100,100,cardT.cardw,cardT.cardh,'/assets/9D.png');
+        /* background */
+        
+        cardT.background = new Image();
+        cardT.background.onload = function(){
+	    cardT.background.ready = true;
+            cardT.timer = setInterval(cardT.drawFrame,40);
+	};
+        cardT.background.src = '/assets/9D.png';
+    },
+    drawFrame: function(){
+        cardT.ctx.clearRect(0,0,cardT.canvas_el.width,cardT.canvas_el.height);
+        cardT.ctx.drawImage(cardT.background,0,0);
+        if(cardT.card) { // if card exists
+            cardT.card.drawImage();
+            cardT.ctx.drawImage(cardT.card.canvas,cardT.card.x,cardT.card.y); 
+        } 
     },
     resize_canvas: function(){
-      cardT.canvas_el.width = window.innerWidth*0.8
+      var inner =  window.innerWidth;
+      cardT.canvas_el.width = inner*0.8
       cardT.canvas_el.height = cardT.canvas_el.width*0.5
+	cardT.cardw = 72 * 640/inner;
+        cardT.cardh = 96 * 640/inner;
     },
     get_table_data: function(){
       var jqXHR= $.get("9/data")
@@ -17,44 +92,18 @@ var cardT = {
     }	
 };
 
-/* propulsion.js sprite engine */
 
+/* on load */
 
-/* on load */ 
 $(function () {
     cardT.setup();
     cardT.resize_canvas();
-(function() {   //  start of propulsion
-	var spr=PP.spr,rm=PP.rm,obj=PP.obj,snd=PP.snd,al=PP.al,global=PP.global,Alarm=PP.Alarm,collision=PP.collision,draw=PP.draw,init=PP.init,key=PP.key,load=PP.load,loop=PP.loop,mouse=PP.mouse,physics=PP.physics,Sound=PP.Sound,SoundEffect=PP.SoundEffect,Sprite=PP.Sprite,view=PP.view,walkDown=PP.walkDown;
-    var can = document.getElementById("main");
-    init("main");
-    spr.c1 = new Sprite('9D.png',1,0,0);
-
-/*
-    load(function(t){
-        obj.card = {
-            initialize: function(){
-              t.x = 100;
-	      t.y = 100;
-              t.sprite = spr.c1;
-	    },
-          draw: function(){
-              t.sprite.draw(t.x,t.y);
-          }
-        };
-	rm.play = function(){
-            loop.register(obj.card);
-        };
-        loop.active = true;
-	loop.room =rm.play();
-    }());
-*/
-}()); // end of propulsion
 });
 
 /* resize canvas when resize window */
 $(window).resize( function(){
- //   console.log('resize');
     cardT.resize_canvas();   
 });
 
+
+   
